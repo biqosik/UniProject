0d98876150a0ecb3fa11ba6d615578350f4a4d0a
biqosik <44440658+biqosik@users.noreply.github.com>
2022-04-09 00:21:47 +0100

    Update

--- UniProject/settings.py
+++ UniProject/settings.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-MEDIA_ROOT = BASE_DIR / 'static/images'
+MEDIA_ROOT = BASE_DIR /  'static/images'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
