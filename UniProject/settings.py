73c60bec23cd71b551fe7426b999637e9680170a
biqosik <44440658+biqosik@users.noreply.github.com>
2022-04-08 19:22:12 +0100

    deploy

--- UniProject/settings.py
+++ UniProject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n$72-wt39tm-r3qwojdh(-h7nmntd0*d8uuc368evdv&yczfx0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'universityprojectbeds']
 
 
 # Application definition
