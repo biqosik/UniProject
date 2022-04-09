597aa2a2ae79c634558a8badfe8b5fd99567d381
biqosik <44440658+biqosik@users.noreply.github.com>
2022-04-09 01:16:34 +0100

    Update

--- UniProject/settings.py
+++ UniProject/settings.py
@@ -123,13 +123,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 django_heroku.settings(locals())
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 MEDIA_URL = '/images/'
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
