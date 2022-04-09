6a05eab3a807904bbc1b9f4d86cadbe58f7f03f4
biqosik <44440658+biqosik@users.noreply.github.com>
2022-04-09 12:37:01 +0100

    Update

--- UniProject/urls.py
+++ UniProject/urls.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('you-would-never-guess-its-admin/', admin.site.urls),
     path('', include('UniversityProject.urls')),
 ]
 
