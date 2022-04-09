ebc78de0f160d0c8be225ea2cd8072469e68dc7f
biqosik <44440658+biqosik@users.noreply.github.com>
2022-04-09 11:16:35 +0100

    Update

--- UniProject/settings.py
+++ UniProject/settings.py
@@ -83,12 +83,8 @@ WSGI_APPLICATION = 'UniProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'Project_Main',
-        'USER': 'postgres',
-        'PASSWORD': 'geraltRyiu2nt3',
-        'HOST':'localhost',
-        'PORT':'5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
